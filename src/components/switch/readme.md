8e378ceda24bbae2055bc42f45330c32f60df4e0
GreenTeaCake <167279+GreenTeaCake@users.noreply.github.com>
2024-04-10 15:33:18 +0300

    Add disabled property

--- src/components/switch/readme.md
+++ src/components/switch/readme.md
@@ -4,14 +4,31 @@
 
 ## Overview
 
-Switch component documentation.
+A switch is an input widget that allows users to choose one of two values: on or off.
+Switches are similar to checkboxes and toggle buttons. But switches can only be used for binary
+input while checkboxes and toggle buttons allow implementations the option of supporting a third
+middle state. Checkboxes can be checked or not checked and can optionally also allow for a
+partially checked state. Toggle buttons can be pressed or not pressed and can optionally allow
+for a partially pressed state.
+
+Specifications:
+
+- [`switch` role](https://w3c.github.io/aria/#switch)
+- [Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+- [Switch Example: A switch based on a div element](https://www.w3.org/WAI/ARIA/apg/patterns/switch/examples/switch/)
+- [ARIA: switch role](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/switch_role)
+
+`gtc-switch` custom element implements the switch specification behaviour. It is a stateless controlled component.
+Please use `checked` property to control the component's state and attach event listener for custom `gtcChange`
+event to be notified about the state changes.
 
 ## Properties
 
-| Property             | Attribute | Description                                                                                               | Type      | Default     |
-| -------------------- | --------- | --------------------------------------------------------------------------------------------------------- | --------- | ----------- |
-| `checked`            | `checked` | The reflective property that allows to control the state of the switch. If `true`, the switch is checked. | `boolean` | `false`     |
-| `label` _(required)_ | `label`   | Text value used for `aria-label` property.                                                                | `string`  | `undefined` |
+| Property             | Attribute  | Description                                                                                               | Type      | Default     |
+| -------------------- | ---------- | --------------------------------------------------------------------------------------------------------- | --------- | ----------- |
+| `checked`            | `checked`  | The reflective property that allows to control the state of the switch. If `true`, the switch is checked. | `boolean` | `false`     |
+| `disabled`           | `disabled` | If `true`, the switch is disabled.                                                                        | `boolean` | `false`     |
+| `label` _(required)_ | `label`    | Text value used for `aria-label` property. The minimal length is 1.                                       | `string`  | `undefined` |
 
 ## Events
 
--- src/components/switch/test/switch.spec.tsx
+++ src/components/switch/test/switch.spec.tsx
@@ -5,12 +5,12 @@ describe('switch', () => {
   it('renders without properties', async () => {
     const page = await newSpecPage({
       components: [Switch],
-      html: `<gtc-switch></gtc-switch>`,
+      html: `<gtc-switch label="Label"></gtc-switch>`,
     });
     expect(page.root).toEqualHtml(`
-      <gtc-switch >
+      <gtc-switch label="Label">
         <mock:shadow-root>
-          <div aria-checked="false" class="gtc-switch" role="switch" tabindex="0">
+          <div aria-checked="false" aria-disabled="false" aria-label="Label" class="gtc-switch" role="switch" tabindex="0">
             <div class="gtc-switch-marker"></div>
           </div>
         </mock:shadow-root>
@@ -18,35 +18,71 @@ describe('switch', () => {
     `);
   });
 
-  it('renders checked', async () => {
-    const page = await newSpecPage({
-      components: [Switch],
-      html: `<gtc-switch checked="true"></gtc-switch>`,
+  describe('checked', () => {
+    it('renders when true', async () => {
+      const page = await newSpecPage({
+        components: [Switch],
+        html: `<gtc-switch label="Label" checked="true"></gtc-switch>`,
+      });
+      expect(page.root).toEqualHtml(`
+        <gtc-switch label="Label" checked="">
+          <mock:shadow-root>
+            <div aria-checked="true" aria-disabled="false" aria-label="Label" class="gtc-switch gtc-switch__checked" role="switch" tabindex="0">
+              <div class="gtc-switch-marker"></div>
+            </div>
+          </mock:shadow-root>
+        </gtc-switch>
+      `);
     });
-    expect(page.root).toEqualHtml(`
-      <gtc-switch checked="">
-        <mock:shadow-root>
-          <div aria-checked="true" class="gtc-switch gtc-switch__checked" role="switch" tabindex="0">
-            <div class="gtc-switch-marker"></div>
-          </div>
-        </mock:shadow-root>
-      </gtc-switch>
-    `);
+
+    it('renders when false', async () => {
+      const page = await newSpecPage({
+        components: [Switch],
+        html: `<gtc-switch label="Label" checked="false"></gtc-switch>`,
+      });
+      expect(page.root).toEqualHtml(`
+        <gtc-switch label="Label" checked="false">
+          <mock:shadow-root>
+            <div aria-checked="false" aria-disabled="false" aria-label="Label" class="gtc-switch" role="switch" tabindex="0">
+              <div class="gtc-switch-marker"></div>
+            </div>
+          </mock:shadow-root>
+        </gtc-switch>
+      `);
+    });
   });
 
-  it('renders unchecked', async () => {
-    const page = await newSpecPage({
-      components: [Switch],
-      html: `<gtc-switch checked="false"></gtc-switch>`,
+  describe('disabled', () => {
+    it('renders when true', async () => {
+      const page = await newSpecPage({
+        components: [Switch],
+        html: `<gtc-switch label="Label" disabled="true"></gtc-switch>`,
+      });
+      expect(page.root).toEqualHtml(`
+        <gtc-switch label="Label" disabled="">
+          <mock:shadow-root>
+            <div aria-checked="false" aria-disabled="true" aria-label="Label" class="gtc-switch gtc-switch__disabled" role="switch" tabindex="0">
+              <div class="gtc-switch-marker"></div>
+            </div>
+          </mock:shadow-root>
+        </gtc-switch>
+      `);
     });
-    expect(page.root).toEqualHtml(`
-      <gtc-switch checked="false">
-        <mock:shadow-root>
-          <div aria-checked="false" class="gtc-switch" role="switch" tabindex="0">
-            <div class="gtc-switch-marker"></div>
-          </div>
-        </mock:shadow-root>
-      </gtc-switch>
-    `);
+
+    it('renders when false', async () => {
+      const page = await newSpecPage({
+        components: [Switch],
+        html: `<gtc-switch label="Label" disabled="false"></gtc-switch>`,
+      });
+      expect(page.root).toEqualHtml(`
+        <gtc-switch label="Label" disabled="false">
+          <mock:shadow-root>
+            <div aria-checked="false" aria-disabled="false" aria-label="Label" class="gtc-switch" role="switch" tabindex="0">
+              <div class="gtc-switch-marker"></div>
+            </div>
+          </mock:shadow-root>
+        </gtc-switch>
+      `);
+    });
   });
 });
